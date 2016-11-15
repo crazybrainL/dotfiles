a8be4d12fb47831d0e44a18165636d92bbcbab4f
whlin <whlin@pro-103.4.1>
2016-11-16 02:58:11 +0800

    Refactor uninstall_dotfiles.sh

--- uninstall_dotfiles.sh
+++ uninstall_dotfiles.sh
@@ -1,33 +1,112 @@
 #!/bin/bash
 
-CURRENT_PATH=`pwd`
-OS_PLATFORM=`uname`
+CLEANRC=all
+
+help() {
+  cat << EOF
+usage: $0 [OPTIONS]
+    -c      Clean dotfilers rc
+
+    example:
+        ./uninstall_dotfiles.sh or ./uninstall_dotfiles.sh -c all
+        ./uninstall_dotfiles.sh -c zsh
+        ./uninstall_dotfiles.sh -c vim
+        ./uninstall_dotfiles.sh -c other
+EOF
+}
+
+cleanZshrc (){
+    FILES=(
+        $HOME/.zsh
+        $HOME/.oh-my-zsh
+        $HOME/.zlogin      
+        $HOME/.zlogout     
+        $HOME/.zprezto     
+        $HOME/.zpreztorc   
+        $HOME/.zprofile    
+        $HOME/.zshenv      
+        $HOME/.zshrc       
+        $HOME/.zshsetting
+    )
+    tLen=${#FILES[@]}
+
+    for (( i=0; i<${tLen}; i++ ));
+    do
+      if [[ -e ${FILES[$i]} ]]; then
+          echo "rm -rf ${FILES[$i]}"
+          rm -rf ${FILES[$i]}
+      else
+          echo "${FILES[$i]} doesn't exist"
+      fi
+    done
+}
+
+cleanVimrc (){
+    FILES=(
+        $HOME/.vim
+        $HOME/.vimrc
+        $HOME/.gvimrc
+        $HOME/.fzf
+    )
+    tLen=${#FILES[@]}
+
+    ~/.fzf/uninstall
+    for (( i=0; i<${tLen}; i++ ));
+    do
+      if [[ -e ${FILES[$i]} ]]; then
+          echo "rm -rf ${FILES[$i]}"
+          rm -rf ${FILES[$i]}
+      else
+          echo "${FILES[$i]} doesn't exist"
+      fi
+    done
+}
+
+cleanOther (){
+    FILES=(
+        $HOME/.subversion
+        $HOME/.gitconfig
+        $HOME/.tmux.conf
+        $HOME/.cgdbrc
+        $HOME/.htoprc
+        $HOME/.screenrc
+        $HOME/.ssh/rc
+        $HOME/.ssh
+        $HOME/.tmux
+        $HOME/.personal_scripts
+    )
+    tLen=${#FILES[@]}
+
+    for (( i=0; i<${tLen}; i++ ));
+    do
+      if [[ -e ${FILES[$i]} ]]; then
+          echo "rm -rf ${FILES[$i]}"
+          rm -rf ${FILES[$i]}
+      else
+          echo "${FILES[$i]} doesn't exist"
+      fi
+    done
+}
+
+while getopts c: opt
+do
+    case $opt in
+        c) CLEANRC=$OPTARG;;
+        \?) echo "Invalid option -$OPTARG" >&2;;
+    esac
+done
 
 # remove the previous files
-rm -rf $HOME/.zsh
-rm -rf $HOME/.oh-my-zsh
-rm -rf $HOME/.zlogin      
-rm -rf $HOME/.zlogout     
-rm -rf $HOME/.zprezto     
-rm -rf $HOME/.zpreztorc   
-rm -rf $HOME/.zprofile    
-rm -rf $HOME/.zshenv      
-rm -rf $HOME/.zshrc       
-rm -rf $HOME/.zshsetting
-rm -rf $HOME/.personal_scripts
 
-rm -rf $HOME/.subversion
-rm -rf $HOME/.gitconfig
-rm -rf $HOME/.tmux.conf
-rm -rf $HOME/.cgdbrc
-rm -rf $HOME/.htoprc
-rm -rf $HOME/.screenrc
-rm -rf $HOME/.ssh/rc
-rm -rf $HOME/.ssh
-rm -rf $HOME/.tmux
+if [ "$CLEANRC" == zsh ]; then
+    cleanZshrc
+elif [ "$CLEANRC" == vim ]; then
+    cleanVimrc
+elif [ "$CLEANRC" == other ]; then
+    cleanOther
+else
+    cleanZshrc
+    cleanVimrc
+    cleanOther
+fi
 
-rm -rf $HOME/.vim
-rm -rf $HOME/.vimrc
-rm -rf $HOME/.gvimrc
-~/.fzf/uninstall
-rm -rf $HOME/.fzf
